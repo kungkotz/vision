960e36421975af85b302fafa342060b7450e3b7c
Alexander <alexander.kocman@geshdo.com>
2023-01-15 00:44:37 +0100

    Add story component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,7 +3,8 @@ import Stories from "./Stories";
 
 function Feed() {
 	return (
-		<main>
+		<main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+			{/* The main element respresents the main content on the webpage, the main content will be displayed using grid and will have 3 breakoints, from 1 to 3 */}
 			<section>
 				<Stories />
 			</section>
--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+import Story from "./Story";
+
 function Stories() {
 	return (
 		<div>
-			<h1>Im a story!</h1>
+			<h1>
+				<Story />
+				<Story />
+				<Story />
+				<Story />
+			</h1>
 		</div>
 	);
 }
