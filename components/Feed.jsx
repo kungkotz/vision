6fff3c9a3a5b610fcf1491ba6769c05bc7d17d32
Alexander <alexander.kocman@geshdo.com>
2023-01-16 23:24:14 +0100

    Conditionally render sideprofile

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,16 +3,24 @@ import Stories from "./Stories";
 import Posts from "./Posts";
 import SideProfile from "./SideProfile";
 import SuggestedProfiles from "./SuggestedProfiles";
+import { useSession } from "next-auth/react";
 
 function Feed() {
+	const { data: session } = useSession();
 	return (
-		<main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
-			<section className=" hidden xl:inline-grid md:col-span-1">
-				<div className="fixed ">
-					<SideProfile />
-					<SuggestedProfiles />
-				</div>
-			</section>
+		<main
+			className={`grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto ${
+				!session && "!grid-cols-1 !max-w-3xl"
+			}`}
+		>
+			{session && (
+				<section className=" hidden xl:inline-grid md:col-span-1">
+					<div className="fixed ">
+						<SideProfile />
+						<SuggestedProfiles />
+					</div>
+				</section>
+			)}
 
 			<section className="col-span-2 md:hidden">
 				<Stories />
--- components/SideProfile.jsx
+++ components/SideProfile.jsx
@@ -1,18 +1,17 @@
-import { useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 function SideProfile() {
 	const { data: session } = useSession();
-	console.log(session);
 	return (
 		<div className="flex items-center justify-between mt-14 ml-10 ">
 			<img
 				className="rounded-full border p-[2px] w-16 h-16"
-				src="https://images.generated.photos/4JcMx1jDIXuHbfBU8clV0RLOv_jT9f3ZAZCLrO7KvRs/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/NzQ0NDIxLmpwZw.jpg"
+				src={session?.user.image}
 				alt="random"
 			/>
 			<div className="flex-1 mx-4">
-				<h2 className="font-bold">Kungkotz</h2>
+				<h2 className="font-bold">{session?.user?.username}</h2>
 				<h3
 					className="text-sm text-gray-400
                 "
@@ -20,7 +19,9 @@ function SideProfile() {
 					Welcome to Vision
 				</h3>
 			</div>
-			<button className="text-red-400 font-bold">Sign Out</button>
+			<button onClick={signOut} className="text-red-400 font-bold">
+				Sign Out
+			</button>
 		</div>
 	);
 }
