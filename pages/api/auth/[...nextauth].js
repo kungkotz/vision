da566653625147b97f96ccd35098f7b30d61fe57
Alexander <alexander.kocman@geshdo.com>
2023-01-22 09:13:38 +0100

    will secret work now ?

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ export const authOptions = {
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			secret: process.env.SECRET,
+			secret: process.env.NEXT_PUBLIC_SECRET,
 		}),
 		// ...add more providers here
 	],
